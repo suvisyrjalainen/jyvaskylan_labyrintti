61de609c16a2c9c1cba92ba2759b999b7c3ddd1e
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2021-03-29 18:46:37 +0300

    liikkuminen tehty

---
+++ Assets/omat_jutut/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController hahmokontrolleri = GetComponent<CharacterController>();
+		
+		float horizontal = Input.GetAxis("Horizontal") * 5;
+        float vertical = Input.GetAxis("Vertical") * 5;
+        Vector3 nopeus = new Vector3(horizontal, 0, vertical);
+		
+		hahmokontrolleri.SimpleMove(nopeus);
+    }
+}

7fd73dd93ec7065c2d09645ed6ac15703e94d22d
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2021-04-12 18:34:23 +0300

    kaantyminen

--- Assets/omat_jutut/Scripts/Player.cs
+++ Assets/omat_jutut/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	private float horisontaalinenPyorinta = 0;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +17,17 @@ public class Player : MonoBehaviour
     {
         CharacterController hahmokontrolleri = GetComponent<CharacterController>();
 		
+		//eteen ja sivulle liikkuminen
 		float horizontal = Input.GetAxis("Horizontal") * 5;
         float vertical = Input.GetAxis("Vertical") * 5;
         Vector3 nopeus = new Vector3(horizontal, 0, vertical);
 		
+		//kääntyminen
+		horisontaalinenPyorinta += Input.GetAxis("Mouse X") * 3;
+		transform.localRotation = Quaternion.Euler(0, horisontaalinenPyorinta, 0);
+        nopeus = transform.rotation * nopeus;
+		
+		//komento, jolla lopulta liikutaan
 		hahmokontrolleri.SimpleMove(nopeus);
     }
 }

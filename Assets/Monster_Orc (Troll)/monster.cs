4697526b2b98a6984625d51c3b7a5c42b3ae6e72
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2021-05-03 18:29:35 +0300

    orkki lisatty

---
+++ Assets/Monster_Orc (Troll)/monster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController hahmokontrolleri = GetComponent<CharacterController>();
+        Vector3 nopeus = new Vector3(0, 0, 1);
+        hahmokontrolleri.Move(nopeus * Time.deltaTime);
+    }
+}
--- Assets/omat_jutut/Scripts/Player.cs
+++ Assets/omat_jutut/Scripts/Player.cs
@@ -5,29 +5,40 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 	private float horisontaalinenPyorinta = 0;
-	
+
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         CharacterController hahmokontrolleri = GetComponent<CharacterController>();
-		
-		//eteen ja sivulle liikkuminen
-		float horizontal = Input.GetAxis("Horizontal") * 5;
+
+        //eteen ja sivulle liikkuminen
+        float horizontal = Input.GetAxis("Horizontal") * 5;
         float vertical = Input.GetAxis("Vertical") * 5;
         Vector3 nopeus = new Vector3(horizontal, 0, vertical);
-		
-		//kääntyminen
-		horisontaalinenPyorinta += Input.GetAxis("Mouse X") * 3;
-		transform.localRotation = Quaternion.Euler(0, horisontaalinenPyorinta, 0);
+
+        //kääntyminen
+        horisontaalinenPyorinta += Input.GetAxis("Mouse X") * 3;
+        transform.localRotation = Quaternion.Euler(0, horisontaalinenPyorinta, 0);
         nopeus = transform.rotation * nopeus;
-		
-		//komento, jolla lopulta liikutaan
-		hahmokontrolleri.SimpleMove(nopeus);
+
+        //komento, jolla lopulta liikutaan
+        hahmokontrolleri.Move(nopeus * Time.deltaTime);
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
     }
 }
